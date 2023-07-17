2876962a3935dbba51264da28d7f61140cd44363
Ruhan van der Berg <35257374+RuhanSA079@users.noreply.github.com>
2023-07-17 15:34:51 +0200

    cmd: bootm: Fixup compile error

--- cmd/bootm.c
+++ cmd/bootm.c
@@ -182,7 +182,7 @@ int do_bootm(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 
 #ifdef CONFIG_FIT
 	case IMAGE_FORMAT_FIT:
-		if (authenticate_image(image_load_addr, fit_get_size((void *)load_addr)) != 0) {
+		if (authenticate_image(image_load_addr, fit_get_size((void *)image_load_addr)) != 0) {
 			printf("Authenticate FIT image Fail, Please check\n");
 			return 1;
 		}
